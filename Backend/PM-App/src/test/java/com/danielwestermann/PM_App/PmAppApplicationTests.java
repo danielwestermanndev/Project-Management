1204aa4362feba8141c2a0ad7b04054928f835be
V1nD3agle <daniel.westermann24@gmail.com>
2025-01-27 21:14:02 +0100

    Initial Commit

---
+++ Backend/PM-App/src/test/java/com/danielwestermann/PM_App/PmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielwestermann.PM_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
