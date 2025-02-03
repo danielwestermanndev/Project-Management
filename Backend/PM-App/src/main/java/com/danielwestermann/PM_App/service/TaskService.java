6287d9bf0623eead760ded7bb8307fc1805386de
V1nD3agle <daniel.westermann24@gmail.com>
2025-02-03 21:15:42 +0100

    Added WebConf and Logging for TaskService

--- Backend/PM-App/src/main/java/com/danielwestermann/PM_App/service/TaskService.java
+++ Backend/PM-App/src/main/java/com/danielwestermann/PM_App/service/TaskService.java
@@ -2,6 +2,8 @@ package com.danielwestermann.PM_App.service;
 
 import com.danielwestermann.PM_App.entity.Task;
 import com.danielwestermann.PM_App.repository.TaskRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,6 +11,7 @@ import java.util.List;
 @Service
 public class TaskService {
     private final TaskRepository taskRepository;
+    private final Logger log = LoggerFactory.getLogger(TaskService.class);
 
     public TaskService(TaskRepository taskRepository){
         this.taskRepository=taskRepository;
@@ -19,6 +22,7 @@ public class TaskService {
     }
 
     public List<Task> findAll(){
+        log.debug("Find All Tasks");
         return taskRepository.findAll();
     }
 
